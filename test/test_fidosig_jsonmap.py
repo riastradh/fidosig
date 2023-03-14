809a19ef3b685ffb513c01b4436f8410947eb70e
Taylor R Campbell <campbell+fidosig@mumble.net>
2023-03-14 01:17:24 +0000

    test/test_fidosig_jsonmap.py: Fix copyright year.

--- test/test_fidosig_jsonmap.py
+++ test/test_fidosig_jsonmap.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-#  Copyright 2020 Taylor R Campbell
+#  Copyright 2023 Taylor R Campbell
 #
 #  Licensed under the Apache License, Version 2.0 (the "License");
 #  you may not use this file except in compliance with the License.
